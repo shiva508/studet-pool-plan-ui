d5aa5f2f859d19e20070b9a306a4d43f019246b4
shiva508 <dasarishiva1@gmail.com>
2021-03-16 20:47:07 +0530

    UI Changes

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,9 @@
+export class Ingredients {
+    name: string = '';
+    amount: number = 0;
+
+    constructor(name: string, amount: number) {
+        this.name = name;
+        this.amount = amount;
+    }
+}
--- src/app/studentfood/shopping-list/shopping-list.component.ts
+++ src/app/studentfood/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredients } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: Ingredients[] = [
+    new Ingredients('Mango', 8),
+    new Ingredients('Sapota',408)
+  ];
   constructor() { }
 
   ngOnInit(): void {
