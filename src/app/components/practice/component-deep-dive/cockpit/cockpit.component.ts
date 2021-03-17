bfbcd63b35c297cb78e01a5b6eea7792e21d7411
shiva508 <dasarishiva1@gmail.com>
2021-03-17 21:20:44 +0530

    @ViewChild and local reference

--- src/app/components/practice/component-deep-dive/cockpit/cockpit.component.ts
+++ src/app/components/practice/component-deep-dive/cockpit/cockpit.component.ts
@@ -1,35 +1,41 @@
-import { Component, EventEmitter, OnInit, Output, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
   templateUrl: './cockpit.component.html',
   styleUrls: ['./cockpit.component.css'],
-  encapsulation:ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None
 })
 export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
   @Output('bpCreated') blueprintCreated = new EventEmitter<{ bluePrintName: string, bluePrintContent: string }>();
   newServerName = '';
   newServerContent = '';
+
+  @ViewChild('localRefServerCOntent', { static: true }) localRefServerCOntent: any;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  onAddServer() {
+  onAddServer(localReferenceServerName: HTMLInputElement) {
+    console.log(localReferenceServerName.value);
+    console.log(this.localRefServerCOntent.nativeElement.value)
     this.serverCreated.emit(
       {
-        serverName: this.newServerName,
-        serverContent: this.newServerContent
+        serverName: localReferenceServerName.value,
+        serverContent: this.localRefServerCOntent.nativeElement.value
       })
     // this.serverElements.push(new SeverModel('server', this.newServerName, this.newServerName));
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(localReferenceServerName: HTMLInputElement) {
+
     this.blueprintCreated.emit(
       {
-        bluePrintName: this.newServerName,
-        bluePrintContent: this.newServerContent
+        bluePrintName: localReferenceServerName.value,
+        bluePrintContent: this.localRefServerCOntent.nativeElement.value
       })
     // this.serverElements.push(new SeverModel('blueprint', this.newServerName, this.newServerName));
   }
